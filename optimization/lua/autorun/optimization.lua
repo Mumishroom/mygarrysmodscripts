ca90f808b7cea886a0da5014ad068da6368a642f
M.H <lifeloverlover3@gmail.com>
2026-02-05 02:04:18 +0200

    Update optimization.lua

--- optimization/lua/autorun/optimization.lua
+++ optimization/lua/autorun/optimization.lua
@@ -9,3 +9,16 @@ timer.Create("decalremover", 30, 0, function()
     RunConsoleCommand("r_cleardecals")
 end)
 
+local function ApplyOptimization()
+    RunConsoleCommand("gmod_mcore_test", "1")
+    RunConsoleCommand("mat_queue_mode", "-1")
+    RunConsoleCommand("r_threaded_particles", "1")
+    RunConsoleCommand("studio_queue_mode", "1")
+    RunConsoleCommand("cl_threaded_bone_setup", "1")
+    RunConsoleCommand("r_queued_ropes", "1")
+end
+
+hook.Add("InitPostEntity", "run_console_commands", function()
+    ApplyOptimization()
+    hook.Remove("OnEntityCreated", "WidgetInit")
+end)
