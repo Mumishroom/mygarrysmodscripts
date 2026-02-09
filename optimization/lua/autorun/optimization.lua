ce44c80595ff7e8503a98c12251115a7c611f7f6
M.H <lifeloverlover3@gmail.com>
2026-02-09 12:06:47 +0200

    Update optimization.lua

--- optimization/lua/autorun/optimization.lua
+++ optimization/lua/autorun/optimization.lua
@@ -1,24 +1,29 @@
-hook.Add("CreateEntityRagdoll", "AutoRemoveAnyRagdoll", function(owner, rag)
-    if IsValid(rag) then
-        rag:Fire("Kill", "", 60)
-    end
-end)
-
-
-timer.Create("decalremover", 30, 0, function()
-    RunConsoleCommand("r_cleardecals")
-end)
-
-local function ApplyOptimization()
-    RunConsoleCommand("gmod_mcore_test", "1")
-    RunConsoleCommand("mat_queue_mode", "-1")
-    RunConsoleCommand("r_threaded_particles", "1")
-    RunConsoleCommand("studio_queue_mode", "1")
-    RunConsoleCommand("cl_threaded_bone_setup", "1")
-    RunConsoleCommand("r_queued_ropes", "1")
+if SERVER then
+    hook.Add("CreateEntityRagdoll", "AutoRemoveAnyRagdoll", function(owner, rag)
+        if IsValid(rag) then
+            rag:Fire("Kill", "", 60)
+        end
+    end)
 end
 
-hook.Add("InitPostEntity", "run_console_commands", function()
-    ApplyOptimization()
-    hook.Remove("OnEntityCreated", "WidgetInit")
-end)
+if CLIENT then
+    local function ApplyOptimization()
+        RunConsoleCommand("gmod_mcore_test", "1")
+        RunConsoleCommand("mat_queue_mode", "-1")
+        RunConsoleCommand("r_threaded_particles", "1")
+        RunConsoleCommand("studio_queue_mode", "1")
+        RunConsoleCommand("cl_threaded_bone_setup", "1")
+        RunConsoleCommand("r_queued_ropes", "1")
+    end
+
+    hook.Add("InitPostEntity", "client_optimization", function()
+        if not timer.Exists("decalremover") then
+            timer.Create("decalremover", 30, 0, function()
+                RunConsoleCommand("r_cleardecals")
+            end)
+        end
+
+        ApplyOptimization()
+        hook.Remove("OnEntityCreated", "WidgetInit")
+    end)
+end
