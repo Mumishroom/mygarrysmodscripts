3e216941c8e1672b9cce12a9e758e5d8d65cecaf
M.H <lifeloverlover3@gmail.com>
2026-02-10 19:28:38 +0200

    Update limit_hp_30.lua

--- limit_hp_30/lua/autorun/limit_hp_30.lua
+++ limit_hp_30/lua/autorun/limit_hp_30.lua
@@ -21,7 +21,7 @@ if SERVER then
 
     hook.Add("ScaleNPCDamage", "OneShotHeadshotNPC", function(npc, hitgroup, dmginfo)
         if hitgroup == HITGROUP_HEAD then
-            ply:SetHealth(0)
+            npc:SetHealth(0)
         end
     end)
 
@@ -44,18 +44,34 @@ if CLIENT then
         return false
     end)
 
-    hook.Add("DrawWorldTip", "Disableworldtip", function()
+    hook.Add("InitPostEntity", "aconsolepart", function()
+        RunConsoleCommand("cl_showhints", "0")
+        RunConsoleCommand("hud_deathnotice_time", "0")
+    end)
+
+    hook.Add( "HUDDrawTargetID", "HidePlayerInfo", function()
         return false
     end)
 
-hook.Add("EntityFireBullets", "NoBulletForce", function(ent, data)
-    data.Force = 0.01
-    return true
-end)
+    local hide = {
+        ["CHudHealth"] = true,
+        ["CHudBattery"] = true,
+        ["CHudAmmo"] = true,
+        ["CHudSecondaryAmmo"] = true,
+        ["CHudCrosshair"] = true
+    }
 
+    hook.Add( "HUDShouldDraw", "HideHUD", function( name )
+        if ( hide[ name ] ) then
+            return false
+        end
+    end )
 
-    hook.Add("InitPostEntity", "aconsolepart", function()
-        RunConsoleCommand("cl_drawhud", "0")
-        RunConsoleCommand("cl_showhints", "0")
+    hook.Add("CalcView", "FuckTFASprintFOV", function(ply, pos, ang, fov)
+        return {
+            origin = pos,
+            angles = ang,
+            fov = GetConVar("fov_desired"):GetInt()
+        }
     end)
 end
